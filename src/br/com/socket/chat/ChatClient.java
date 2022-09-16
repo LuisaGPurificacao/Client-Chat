745de710c586be1f5d20e9dd3db80640f6c21881
LuisaGPurificacao <luisa.purificacao16@gmail.com>
2022-09-16 17:27:33 -0300

    Projeto inicial chat

---
+++ src/br/com/socket/chat/ChatClient.java
@@ -0,0 +1,45 @@
+package br.com.socket.chat;
+
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+
+public class ChatClient {
+
+	public static void main(String[] args) {
+
+		try {
+			Socket socket = new Socket("localhost", 9090);
+			DataInputStream in = new DataInputStream(socket.getInputStream());
+			DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+			String messageReceived = "", messageToSend = "";
+
+			while (true) {
+				messageToSend = br.readLine();
+				out.writeUTF(messageToSend);
+				out.flush();
+
+				if (messageToSend.equals("exit"))
+					break;
+
+				messageReceived = in.readUTF();
+				System.out.println("Server says: " + messageReceived);
+			}
+
+			br.close();
+			out.close();
+			in.close();
+			socket.close();
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
---
+++ src/br/com/socket/chat/ChatServer.java
@@ -0,0 +1,50 @@
+package br.com.socket.chat;
+
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class ChatServer {
+
+	public static void main(String[] args) {
+
+		try {
+			ServerSocket serverSocket = new ServerSocket(9090);
+			System.out.println("Server is ready to chat!");
+
+			Socket socket = serverSocket.accept();
+			DataInputStream in = new DataInputStream(socket.getInputStream());
+			DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+			String messageReceived = "", messageToSend = "";
+
+			while (true) {
+				messageReceived = in.readUTF();
+				System.out.println("Client says: " + messageReceived);
+
+				if (messageReceived != null && messageReceived.equals("exit"))
+					break;
+
+				messageToSend = br.readLine();
+				out.writeUTF(messageToSend);
+
+				out.flush();
+			}
+
+			br.close();
+			out.close();
+			in.close();
+			serverSocket.close();
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
